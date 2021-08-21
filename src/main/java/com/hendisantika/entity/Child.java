2fefd45516bb72c4a83e88e9a40ca6abad1dddfb
Hendi Santika <hendisantika@gmail.com>
2021-08-21 22:50:38 +0700

    #10 - Add Child Entity

---
+++ src/main/java/com/hendisantika/entity/Child.java
@@ -0,0 +1,23 @@
+package com.hendisantika.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jsonb-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/08/21
+ * Time: 22.50
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Child implements Serializable {
+    private String name;
+}
