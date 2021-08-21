19099ae990bae9a6a033eb81404c7612fe7bb901
Hendi Santika <hendisantika@gmail.com>
2021-08-21 22:45:25 +0700

    #8 - Add AccountRepositoryTest

---
+++ src/test/java/com/hendisantika/repository/AccountRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.hendisantika.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jsonb-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/08/21
+ * Time: 22.44
+ */
+@SpringBootTest
+public class AccountRepositoryTest {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+}
