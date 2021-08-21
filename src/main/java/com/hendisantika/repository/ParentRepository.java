5e81250cc10c7505ea820552f11b27777574bcb8
Hendi Santika <hendisantika@gmail.com>
2021-08-21 22:52:16 +0700

    #13 - Add ParentRepository

---
+++ src/main/java/com/hendisantika/repository/ParentRepository.java
@@ -0,0 +1,17 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.Parent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jsonb-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/08/21
+ * Time: 22.51
+ */
+public interface ParentRepository extends JpaRepository<Parent, Integer> {
+
+}
