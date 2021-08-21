3f21da5dc48943d3522a793bde95be2889fe22dc
Hendi Santika <hendisantika@gmail.com>
2021-08-21 22:48:29 +0700

    #9 - Add Test Case in AccountRepositoryTest

--- src/test/java/com/hendisantika/repository/AccountRepositoryTest.java
+++ src/test/java/com/hendisantika/repository/AccountRepositoryTest.java
@@ -1,7 +1,14 @@
 package com.hendisantika.repository;
 
+import com.hendisantika.entity.Account;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +25,20 @@ public class AccountRepositoryTest {
     @Autowired
     AccountRepository accountRepository;
 
+    @BeforeEach
+    public void setUp() throws Exception {
+        HashMap<String, Object> info = new HashMap<>();
+        info.put("name", "Uzumaki Naruto");
+        info.put("age", 3);
+        info.put("tel", "110");
+        info.put("Hobby", Arrays.asList("Reading", "Coding", "Browsing", "Watching Movies"));
+        Account account = new Account().setInfo(info);
+        accountRepository.save(account);
+    }
+
+    @Test
+    public void findByName() {
+        List<Account> accounts = accountRepository.findByName("Uzumaki Naruto");
+        System.out.println(accounts);
+    }
 }
